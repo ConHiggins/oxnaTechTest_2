9d2a5942bd0a2da24c23ea769f1f2f3f81210ed2
Conor Higgins <wodowods@gmail.com>
2022-07-11 13:52:03 +0100

    functioning english > morse

--- app.js
+++ app.js
@@ -6,23 +6,34 @@ const button = document.querySelector(".output-button");
 const answer = document.querySelector(".output");
 
 
-const translate = (input) => {
+export const translate = (input) => {
 
-    let individualLetters = Array.from(input.split(""));
-    console.log(individualLetters);
+    let outputArr = [];
+    let individualLetters = Array.from(input.toUpperCase().split(""));
+    
+    for(let i=0; i<individualLetters.length; i++) {
+
+        let ASCIIchar = individualLetters[i].charCodeAt(0);
+        if((ASCIIchar >= 65 && ASCIIchar <= 90) || (ASCIIchar >= 48 && ASCIIchar <= 57))
+            {outputArr.push(morseArray[ASCIIchar-65]);
+            outputArr.push(" ");}
+        
+            
+       
+    }
+
+    let result =  outputArr.join("");
+
+    return result;
 
 }
 
-const printTranslate = (input) => {
+export const printTranslate = (input) => {
 
     console.log(input);
     return answer.innerText = String(translate(input));
 
 }
-
-//button.addEventListener("click", printTranslate.bind(userInput));
-
-
 
 button.addEventListener("click", () => {
 
---
+++ app.test.js
@@ -0,0 +1,9 @@
+import { translate, printranslate } from "./app.js";
+
+
+it("returns morse code", () => {
+
+    printranslate("a1");
+
+    expect(result).toBe(".-.----");
+})
